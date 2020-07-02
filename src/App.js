b905fe62b7cae1bfc14ee50c1342756f03197dc4
Yuri Alves Miguel <yuri.snp@hotmail.com>
2020-07-01 21:19:37 -0300

    Editing Contact page layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Routes from "./routes";
-import "./Global.css";
+import "./global.css";
 
 function App() {
   return (
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,54 +1,59 @@
 import React from "react";
 import "./styles.css";
 import Header from "../../components/Header";
-import { FaWhatsapp, FaFacebookSquare, FaInstagram } from "react-icons/fa";
+import detail from "../../assets/detail.svg";
+import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa";
 
 function Contact() {
   return (
     <>
       <Header />
       <div className="contactContainer">
-        <div className="inputable">
-          <p>Entre em contato para mais informações:</p>
-          <input type="text" placeholder="Digite seu nome" />
-          <input type="email" placeholder="Email" />
-          <input type="text" placeholder="Telefone" />
+        <form action="">
+          <div className="containerUpContact">
+            <div className="title">
+              <h3>Entre em contato para mais informações:</h3>
+              <input type="text" placeholder="Nome" />
+              <input type="email" placeholder="Email" />
+              <input type="text" placeholder="Telefone" />
+            </div>
+            <div className="infos">
+              <ul className="logos">
+                <li>
+                  <a
+                    target="_blank"
+                    href="https://api.whatsapp.com/send?phone=5545991023801&text=Ol%C3%A1%2C%20vim%20pelo%20seu%20site!"
+                  >
+                    <FaWhatsapp size={25} />
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://facebook.com">
+                    <FaFacebook size={25} />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    href="https://www.instagram.com/fernandaisayu/"
+                  >
+                    <FaInstagram size={25} />
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <br />
+                <li>Odontoclin Clínica Odontológica</li>
+                <li>Rua Sete de Setembro, 2325 - Centro</li>
+                <li>CEP: 85801-140 | Cascavel, PR</li>
+                <li>Telefone: (45) 3035-1009 e (45) 99102-3801</li>
+              </ul>
+            </div>
+          </div>
           <textarea type="text" placeholder="Mensagem" />
           <button type="submit">Enviar Email</button>
-        </div>
-        {/* <div className="otherInfor">
-          <ul className="logos">
-            <li>
-              <a
-                target="_blank"
-                href="https://api.whatsapp.com/send?phone=5545991023801&text=Ol%C3%A1%2C%20vim%20pelo%20seu%20site!"
-              >
-                <FaWhatsapp />
-              </a>
-            </li>
-            <li>
-              <a target="_blank" thref="">
-                <FaFacebookSquare />
-              </a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://www.instagram.com/fernandaisayu/"
-              >
-                <FaInstagram />
-              </a>
-            </li>
-          </ul>
-          <ul>
-            <br />
-            <li>Odontoclin Clínica Odontológica</li>
-            <br />
-            <li>Rua Sete de Setembro, 2325 - Centro</li>
-            <li>CEP: 85801-140 | Cascavel, PR</li>
-            <li>Telefone: (45) 3035-1009 e (45) 99102-3801</li>
-          </ul>
-        </div> */}
+        </form>
+        <img className="detail" src={detail} alt="detail" />
       </div>
     </>
   );
